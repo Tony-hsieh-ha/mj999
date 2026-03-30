2d04c1608b4e27fe13fe7c2543b93f89909574ca
Tony-hsieh-ha <nctu6814@gmail.com>
2026-03-31 07:18:27 +0800

    Add files via upload

--- api.js
+++ api.js
@@ -480,7 +480,7 @@ class APIRoutes {
             // LINE Token 交換設定
             const LINE_CONFIG = {
                 clientId: '2004473747',
-                clientSecret: 'a5ef8b23a930a8320c273f21badc78c2', // 直接寫死的 LINE Channel Secret
+                clientSecret: process.env.LINE_CLIENT_SECRET || 'a5ef8b23a930a8320c273f21badc78c2',
                 redirectUri: 'https://mj999-2168.vercel.app/' // 確認最後面有一個斜線，與 LINE 後台一致
             };
             
